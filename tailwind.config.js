52bfec2fb7d11e4ef01010af0a0aff2d894d2ea3
Mike Pham <mikep@xello.world>
2024-04-23 23:49:19 -0400

    learning about layer css attribute & setup some custom themes on tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,36 @@ module.exports = {
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      // colors: {
+      //   primary: {
+      //     dark: '#CC00CC',
+      //     DEFAULT: 'rebeccapurple'
+      //   }
+      // },
+      colors(theme) {
+        return {
+          primary: {
+            ...theme.colors.purple,
+            dark: '#CC00CC',
+            DEFAULT: 'rebeccapurple'
+          },
+          'electric-violet': {
+            '50': '#f5f2ff',
+            '100': '#eee7ff',
+            '200': '#ded3ff',
+            '300': '#c5afff',
+            '400': '#aa81ff',
+            '500': '#914fff',
+            '600': '#852bfc',
+            '700': '#7b20e9',
+            '800': '#6314c3',
+            '900': '#53139f',
+            '950': '#32096c',
+          },
+        }
+      }
+    },
   },
   plugins: [],
 }
